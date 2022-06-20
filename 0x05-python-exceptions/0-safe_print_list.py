f650f302dc37cf074ca3146c4d89e77fe14db406
OPunga <ronnyopunga96@gmail.com>
2022-06-20 11:44:08 -0700

    ME

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while i < x:
+        try:
+            print(my_list[i], end="")
+        except(IndexError, ValueError, TypeError):
+            break
+        else:
+            i += 1
+            print("")
+            return i
