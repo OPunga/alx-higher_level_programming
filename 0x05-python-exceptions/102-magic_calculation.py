751207f5a7abeda0002631f6ca5fa22ff4585781
OPunga <ronnyopunga96@gmail.com>
2022-06-20 13:42:51 -0700

    ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    var = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                var += (a**b) / i
+        except:
+            var = (a + b)
+            break
+    return var
