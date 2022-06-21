673df956cebc02e0eec177de4934d5f03412573e
OPunga <ronnyopunga96@gmail.com>
2022-06-21 11:14:17 -0700

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 class square:
-    def __init__(self. height=0, width=0):
-        self.height = height
-        self.width = width
-        def height
+    """defines an empty class"""
+    pass
