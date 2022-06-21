5b67fc222d4a9862f5296e1343b9985dbb2b79a7
OPunga <ronnyopunga96@gmail.com>
2022-06-21 13:07:24 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """
+        Creates an instance od Square
+        Args:
+            size: size of the square
+        """
+        self.__size = size
