080b3a08adcc5b4f3601eaf15a6b015fa1f279ec
OPunga <ronnyopunga96@gmail.com>
2022-06-28 10:36:20 -0700

    Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/ussr/bin/bash
+"""
+    101-locked_class: class LockedClass
+"""
+
+class LockedClass:
+    """
+    A class that can only have one attribute first_name.
+    Attribute:
+         first_name (str): name
+    """
+    __slots__ = ['first_name']
