1498822882f9573c2c0275ddfa7af59600694f68
OPunga <ronnyopunga96@gmail.com>
2022-06-28 10:46:00 -0700

    LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,13 @@
-#!/ussr/bin/bash
+#!/usr/bin/python3
 """
     101-locked_class: class LockedClass
 """
 
+
 class LockedClass:
     """
-    A class that can only have one attribute first_name.
-    Attribute:
-         first_name (str): name
+        A class that can only have one attribute first_name.
+        Attribute:
+             first_name (str): name
     """
     __slots__ = ['first_name']
