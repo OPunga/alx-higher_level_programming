97d049d7a6e46fc6edb7c1d4658a18c45a5032a1
OPunga <ronnyopunga96@gmail.com>
2022-07-04 21:45:39 +0000

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Ronny"""
+
+
+def is_same_class(obj, a_class):
+    """if object is an istanace of a class
+    args:
+    object being
+    class attribute
+    """
+
+
+if type(obj) is a_class:
+    return True
+else:
+    False
