fcbce7f7f83b374f3acb01efa19657d9c8c3eb6b
OPunga <ronnyopunga96@gmail.com>
2022-07-05 20:11:30 +0000

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Write to a file"""
+"""file reader"""
 
 
 def write_file(filename="", text=""):
-    """writes a text to a file"""
+    """wriites a file"""
 
-    with open(filename, "w" ,encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return (file.write(text))
