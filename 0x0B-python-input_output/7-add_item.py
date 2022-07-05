31f1a8eb01f1b406314a0e8e476aa44b3e56448f
OPunga <ronnyopunga96@gmail.com>
2022-07-05 23:33:30 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,20 +2,22 @@
 """load add save"""
 
 import sys
+
+
 def ronny():
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    save_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_json_file = __import__("6-load_from_json_file").load_from_json_file
 
     file = "add_item.json"
     json_list = []
 
     try:
-        load_from_json_file(file)
-    except:
+        load_json_file(file)
+    except FileNotFoundError:
         json_list = []
 
     json_list.extend(sys.argv[1:])
-    save_file = save_to_json_file(json_list, file)
+    save_file = save_json_file(json_list, file)
     return save_file
 
 
