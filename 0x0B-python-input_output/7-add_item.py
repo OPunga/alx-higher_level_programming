cae8244fc2cb543f3d862afd577f8d1ab3d7f7f7
OPunga <ronnyopunga96@gmail.com>
2022-07-05 23:37:45 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,8 @@ import sys
 
 
 def ronny():
+    """a sscript that adds arguments to a python file"""
+
     save_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_json_file = __import__("6-load_from_json_file").load_from_json_file
 
