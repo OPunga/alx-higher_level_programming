f55753b42b1155bf0ec75b67fe9770c0e5d9ee0d
OPunga <ronnyopunga96@gmail.com>
2022-07-06 00:17:55 +0000

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""student to JSON"""
+
+
+class Student:
+    """a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
