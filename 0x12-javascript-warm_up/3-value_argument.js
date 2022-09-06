c8df49fdb7dc073210772046a53ee99ecd0935e7
OPunga <ronnyopunga96@gmail.com>
2022-09-06 11:22:50 +0300

    prints argument passed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let args = process.argv;
+let count = 0
+
+for(i in args){
+	count += 1
+}
+if (count === 2){
+	console.log("No argument");
+}
+else if(count > 2){
+	console.log(args[2]);
+}
