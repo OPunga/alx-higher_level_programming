6e4434655f081ed0189c5fd29fa809b9adea03e0
OPunga <ronnyopunga96@gmail.com>
2022-09-15 15:02:31 +0300

    injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python3
-
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
 
 import MySQLdb
 from sys import argv
 
-if __name__=='__main__':
-    """Selecting a database"""
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-    db = MySQLdb.connect(host="localhost",user=argv[1], port=3306,passwd=argv[2],db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-            WHERE name LIKE BINARY %(name)s \
-            ORDER BY states.id ASC",{'name': argv[4]})
-    rows = cur.fetchall()
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+
+        rows = cur.fetchall()
+
     if rows is not None:
         for row in rows:
             print(row)
