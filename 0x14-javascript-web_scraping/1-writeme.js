66e8d82e53c9e6eae9a999199516a1e1c7077b8c
OPunga <ronnyopunga96@gmail.com>
2022-10-18 23:34:34 +0300

    write a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
-
-fs.writeFile(file, content, 'utf-8', function(err){
-	if (err){
-		console.log(err);
-	}
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
-
